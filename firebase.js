16057d804feb7335f758e80af37eae93b87c72f0
David <grantclan.7@gmail.com>
2025-03-24 14:56:39 +0000

    prepared for vercel deploy

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBnjoG5-MvBdFoYF1fd-qXPguSH2VUTMmw",
-    authDomain: "amzn2-f8e83.firebaseapp.com",
-    projectId: "amzn2-f8e83",
-    storageBucket: "amzn2-f8e83.firebasestorage.app",
-    messagingSenderId: "774461316507",
-    appId: "1:774461316507:web:6d9bc91fc4dc47932dd53e"
-  };
+  apiKey: "AIzaSyBnjoG5-MvBdFoYF1fd-qXPguSH2VUTMmw",
+  authDomain: "amzn2-f8e83.firebaseapp.com",
+  projectId: "amzn2-f8e83",
+  storageBucket: "amzn2-f8e83.firebasestorage.app",
+  messagingSenderId: "774461316507",
+  appId: "1:774461316507:web:6d9bc91fc4dc47932dd53e"
+};
