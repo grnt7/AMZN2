bd9d6c9e6b7a6e564de554c84dfb18beb7615f72
David <grantclan.7@gmail.com>
2026-02-24 21:39:55 +0000

    pages/index.js syntax error resolved

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,9 +25,15 @@ export default function Home({products}) {
 }
 
 export async function getServerSideProps(context) {
-  const products = await fetch(" https://fakestoreapi.com/products").then(
-    (res) => res.json()
-  );
+ const res = await fetch("https://fakestoreapi.com/products");
+  
+  // 2. Added a check to ensure we actually got JSON back
+  if (!res.ok) {
+    console.error("Failed to fetch products", res.status);
+    return { props: { products: [] } }; 
+  }
+
+  const products = await res.json();
 
   return {
     props: {
@@ -38,38 +44,7 @@ export async function getServerSideProps(context) {
 
  
 
-/*
-  {/* ---- TO BEGIN, delete this section and GET CODING!!! ---- }
-  <center className="grid place-items-center mt-10">
-  <h1 className="text-5xl">Lets build Amazon 2.0</h1>
-  <h2>This is your starter template!</h2>
-  <br />
-  <h3 className="font-bold">
-    We will be using Next.js / Tailwind CSS / Redux / Firebase / NextAuth
-  </h3>
-  <i>(Dont worry, its all setup and ready to use!)</i>
-  <h4>Get Ready, Get Set, GO!!!</h4>
-
-  <h5 className="mb-10">#PAPAFAM</h5>
-
-  <div className="bg-red-300 p-10">
-    <p className="font-bold">
-      Dont forget to register for the challenge here!
-    </p>
-    <p>👇👇👇</p>
-    <a
-      href="https://www.papareact.com/secret-challenge"
-      className="text-blue-400 underline p-3 font-bold"
-    >
-      CLICK HERE TO REGISTER NOW
-    </a>
-  </div>
-
-  <p className="mt-24">Built with 💙 by Sonny Sangha (PAPA REACT)</p>
-</center>
-{ ---- ---- }
 
 
 
 
-*/
